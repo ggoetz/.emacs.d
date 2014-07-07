b39c53d39f7b462e10240b1dc4749d88967876a1
Georges Goetz <ggoetz@stanford.edu>
2014-07-06 17:56:44 -0700

    Initial python settings file

--- settings/python-settings.el
+++ settings/python-settings.el
@@ -3,34 +3,11 @@
 ;;; Commentary:
 
 ;;; Code:
-; python-mode
-(setq py-install-directory "~/.emacs.d/python-mode.el-6.1.3")
-(add-to-list 'load-path py-install-directory)
-(require 'python-mode)
 
-; set python console args
-(setq
- python-shell-interpreter "ipython"
- python-shell-interpreter-args (if (system-is-mac)
-"--gui=osx --matplotlib=osx --colors=Linux"
-                                   (if (system-is-linux)
-"--gui=wx --matplotlib=wx --colors=Linux"))
- python-shell-prompt-regexp "In \\[[0-9]+\\]: "
- python-shell-prompt-output-regexp "Out\\[[0-9]+\\]: "
- python-shell-completion-setup-code
-   "from IPython.core.completerlib import module_completion"
- python-shell-completion-module-string-code
-   "';'.join(module_completion('''%s'''))\n"
- python-shell-completion-string-code
-   "';'.join(get_ipython().Completer.all_completions('''%s'''))\n")
+; Enable elpy mode
+(package-initialize)
+(elpy-enable)
 
-; switch to the interpreter after executing code
-(setq py-shell-switch-buffers-on-execute-p t)
-(setq py-switch-buffers-on-execute-p t)
-; don't split windows
-(setq py-split-windows-on-execute-p nil)
-; try to automagically figure out indentation
-(setq py-smart-indentation t)
-
+ 
 (provide 'python-settings)
 ;;; python-settings.el ends here
